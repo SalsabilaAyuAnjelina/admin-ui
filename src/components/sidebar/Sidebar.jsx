cddd4a05c095a1e53eaa353b6063761edb51e6d4
SalsabilaAyuAnjelina <ayuanjelina97@gmail.com>
2024-04-10 00:32:40 +0700

    first commit

---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,62 @@
+import "./sidebar.scss";
+import { Link } from "react-router-dom";
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
+import CreditCardOutlinedIcon from '@mui/icons-material/CreditCardOutlined';
+import StoreRoundedIcon from '@mui/icons-material/StoreRounded';
+import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
+import LogoutOutlinedIcon from '@mui/icons-material/LogoutOutlined';
+
+const Sidebar = () => {
+  return (
+    <div className="sidebar">
+      <div className="top">
+        <Link to="/">
+          <span className="logo">Store</span>
+        </Link>
+      </div>
+      <hr />
+      <div className="center">
+        <ul>
+          <p className="title">MAIN</p>
+          <li>
+            <DashboardIcon className="icon" />
+            <span>Dashboard</span>
+          </li>
+          <p className="title">LIST</p>
+          <Link to="/users">
+            <li>
+              <PersonOutlineOutlinedIcon className="icon" />
+              <span>Users</span>
+            </li>
+          </Link>
+          <Link to="/products">
+            <li>
+              <CreditCardOutlinedIcon className="icon" />
+              <span>Products</span>
+            </li>
+          </Link>
+          <li>
+            <StoreRoundedIcon className="icon" />
+            <span>Orders</span>
+          </li>
+          <p className="title">USER</p>
+          <li>
+            <AccountCircleOutlinedIcon className="icon" />
+            <span>Profile</span>
+          </li>
+          <li>
+            <LogoutOutlinedIcon className="icon" />
+            <span>Logout</span>
+          </li>
+        </ul>
+      </div>
+      <div className="bottom">
+        <div className="colorOption"></div>
+        <div className="colorOption"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
